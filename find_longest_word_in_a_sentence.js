ae81644e2f6a85977ad65fc2e440572e514afbec
Huno <136697789+hunxio@users.noreply.github.com>
2023-12-04 14:55:55 +0100

    Testing git commits through VSC

--- find_longest_word_in_a_sentence.js
+++ find_longest_word_in_a_sentence.js
@@ -1,19 +1,22 @@
 /*
-*  Return the length of the longest word in the provided sentence. 
-*  Your response should be a number.
-*/
+ *  Return the length of the longest word in the provided sentence.
+ *  Your response should be a number.
+ */
 
 function findLongestWordLength(str) {
-    let result = 0;
-    const wordDefinition = /\w+/g;
-    const words = str.match(wordDefinition);
-    for (let i = 0; i < words.length; i++) {
-      if (result < words[i].length) {
-        result = words[i].length;
-    };
-  };
+  let result = 0;
+  const wordDefinition = /\w+/g;
+  const words = str.match(wordDefinition);
+  for (let i = 0; i < words.length; i++) {
+    if (result < words[i].length) {
+      result = words[i].length;
+    }
+  }
   return result;
 }
-  
 
-console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"));
+console.log(
+  findLongestWordLength(
+    "What if we try a super-long word such as otorhinolaryngology"
+  )
+);
